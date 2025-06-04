34d44ac37da0dcc3aa7aeab84ea2468bf6887239
Felix <felixmcwong@gmail.com>
2025-06-04 18:11:16 +1200

    Add City managing methods

--- TicketBookingApp/Program.cs
+++ TicketBookingApp/Program.cs
@@ -36,10 +36,51 @@ namespace TicketBookingApp
             }
         }
 
-        public List<> GetTemplate()
+        public List<City> CitiesSelect()
         {
-            List<> list = new List<>();
-            string sqlString = "SELECT * FROM "
+            List<City> cities = new List<City>();
+            string sqlString = "SELECT * FROM ";
+
+            using (SqlCommand cmd = new SqlCommand(sqlString, connection))
+            {
+                using (SqlDataReader reader = cmd.ExecuteReader())
+                {
+                    while (reader.Read())
+                    {
+                        int cityId = Convert.ToInt32(reader["cityId"]);
+                        string cityName = reader["cityName"].ToString();
+                        cities.Add(new City(cityId, cityName));
+                    }
+                }
+            }
+            return cities;
+        }
+
+        public void CitiesInsert(City city)
+        {
+            string sqlString = "INSERT INTO _ VALUES ";
+            using (SqlCommand cmd = new SqlCommand(sqlString, connection))
+            {
+
+            }
+        }
+
+        public void CitiesDelete(City city)
+        {
+            string sqlString = "DELETE FROM _ WHERE";
+            using (SqlCommand cmd = new SqlCommand(sqlString, connection))
+            {
+
+            }
+        }
+
+        public void CitiesUpdate(City oldCity, City newCity)
+        {
+            string sqlString = "UPDATE _ SET ";
+            using (SqlCommand cmd = new SqlCommand(sqlString, connection))
+            {
+
+            }
         }
     }
 }
