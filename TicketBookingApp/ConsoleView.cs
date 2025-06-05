bbc66e7656f675c8691c562df7a2dd88ff54f61d
Felix <felixmcwong@gmail.com>
2025-06-05 18:31:28 +1200

    Created ConsoleView class file

---
+++ TicketBookingApp/ConsoleView.cs
@@ -0,0 +1,31 @@
+﻿namespace TicketBookingApp
+{
+    public class ConsoleView
+    {
+        private int WindowWidth = Console.WindowWidth;
+        private int WindowHeight = Console.WindowHeight;
+
+        public void LogInScreen()
+        {
+            Console.Clear();
+            DrawHeader();
+            DrawFooter();
+        }
+
+        private void DrawHeader()
+        {
+            string header = "Ticket Sales Application";
+            int horizontalPos = (int)Math.Round((WindowWidth / 2d) - (header.Length / 2d));
+            Console.SetCursorPosition(horizontalPos, 0);
+            Console.WriteLine(header);
+        }
+
+        private void DrawFooter()
+        {
+            string footer = "Exit - Ctrl + E";
+            int horizontalPos = (int)Math.Round((WindowWidth / 2d) - (footer.Length / 2d));
+            Console.SetCursorPosition(horizontalPos, WindowHeight - 1);
+            Console.WriteLine(footer);
+        }
+    }
+}
