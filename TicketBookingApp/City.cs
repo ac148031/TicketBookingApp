75cd0fdf21177f575025b0bf273f2c5dbd41c906
Felix <felixmcwong@gmail.com>
2025-06-05 18:27:01 +1200

    CIty Table Class

---
+++ TicketBookingApp/City.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TicketBookingApp
+{
+    public class City
+    {
+        public int CityId { get; set; }
+        public string CityName { get; set; }
+
+        public City(int cityId, string cityName)
+        {
+            CityId = cityId;
+            CityName = cityName;
+        }
+    }
+}
