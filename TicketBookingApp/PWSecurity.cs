34c2df05ae887b60b44b2711956c595a6bb74a19
Felix <felixmcwong@gmail.com>
2025-06-06 12:23:09 +1200

    Created class to Hash and Verify passwords

---
+++ TicketBookingApp/PWSecurity.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace TicketBookingApp
+{
+    public class PWSecurity
+    {
+        public static string Hash(string source)
+        {
+            string hash = BCrypt.Net.BCrypt.EnhancedHashPassword(source, 13);
+
+            return hash.ToString();
+        }
+
+        public static bool Verify(string password, string hash)
+        {
+            return BCrypt.Net.BCrypt.EnhancedVerify(password, hash);
+        }
+    }
+}
