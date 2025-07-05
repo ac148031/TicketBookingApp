3e859f37486537473f2818e70941f7613f9e2fc4
Felix <felixmcwong@gmail.com>
2025-07-05 12:11:48 +1200

    Moved files so theyre visible in VS2022 solution explorer

---
+++ TicketBookingApp/TicketBookingDatabase/TicketBookingDatabase/Test.sql
@@ -0,0 +1,9 @@
+﻿declare @search varchar(max);
+set @search = '1234';
+
+select * from sales.tblCustomers
+WHERE customerFirstName like @search + '%' 
+    OR customerLastName like @search + '%' 
+    OR customerEmail like @search + '%' 
+    --OR customerPhone like @search + '%' 
+    OR Cast(customerId as varchar) like @search + '%' 
